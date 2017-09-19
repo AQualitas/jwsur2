022a7977ad9958844bb747283b766258b6c048d1
andres <andres.guerrerog@gmail.com>
2017-09-19 18:58:00 +0200

    19/09/2017

---
+++ JWSURCh4RandomClient/src/com/jwsur2/ch4/client/Next1Response.java
@@ -0,0 +1,54 @@
+
+package com.jwsur2.ch4.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para next1Response complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="next1Response">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "next1Response", propOrder = {
+    "_return"
+})
+public class Next1Response {
+
+    @XmlElement(name = "return")
+    protected int _return;
+
+    /**
+     * Obtiene el valor de la propiedad return.
+     * 
+     */
+    public int getReturn() {
+        return _return;
+    }
+
+    /**
+     * Define el valor de la propiedad return.
+     * 
+     */
+    public void setReturn(int value) {
+        this._return = value;
+    }
+
+}
---
+++ JWSURCh4RandomClient/src/com/jwsur2/ch4/client/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://random.ch4.jwsur2.com/")
+package com.jwsur2.ch4.client;
